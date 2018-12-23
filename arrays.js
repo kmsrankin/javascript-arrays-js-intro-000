befc7cfc68649921affa1ad9dab695f959b59083
Keegan Rankin <kms.rankin@gmail.com>
2018-12-23 17:16:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,10 +5,10 @@ var chocolateBars = [
   "skittles",
 ];
 
-function addElementToBeginningOfArray(array, element) {
-  ["foo", ...chocolateBars]
+function addElementToBeginningOfArray(array = chocolateBars, element = "mound") {
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  chocolateBars.unshift("foo")
+  return chocolateBars.unshift("foo")
 }
