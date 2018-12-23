3189a945557727898e22aa984e33e747df5269bc
Keegan Rankin <kms.rankin@gmail.com>
2018-12-23 17:08:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,5 +10,5 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return chocolateBars.push("poopbar")
+  return chocolateBars.push("foo")
 }
